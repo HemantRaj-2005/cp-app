b8e94aef8fb2714e478605675ae9f2a44bc61506
Hemant Raj <coder.hemantraj05@gmail.com>
2024-06-08 16:45:13 +0530

    Modified Authentication page

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -15,10 +15,14 @@ const AuthPage = () => {
       <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl">
         <div className="flex flex-col items-center justify-center p-8 md:p-0 md:w-1/2">
           <img src={logo} alt="SkillCrafter Logo" className="w-20 h-20 mb-4 cursor-pointer" onClick={() => window.location.href = '/'} />
-          <h1 className="text-3xl font-bold mb-4">Welcome to SkillCrafter</h1>
+          <h1 className="text-3xl font-bold mb-4">Welcome to
+            <span className='px-2 py-1 bg-gradient-to-r from-purple-900 via-indigo-900 to-gray-950 rounded-lg text-white shadow-2xl transition-transform hover:scale-105' >
+              SkillCrafter
+            </span>
+          </h1>
           <p className="text-lg">Craft your skills with us and ace your coding journey!</p>
         </div>
-        <div className="bg-black p-8 rounded-lg shadow-lg w-full md:w-1/2">
+        <div className="bg-gradient-to-r from-slate-950 via-cyan-950 to-zinc-950 p-8 rounded-lg shadow-lg w-full md:w-1/2">
           {isLogin ? <LoginForm toggleForm={toggleForm} /> : <RegisterForm toggleForm={toggleForm} />}
         </div>
       </div>
